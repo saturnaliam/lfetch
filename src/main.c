5ef2bca708e726e8c5c27e5a85f3659be311d598
saturnaliam <luciacdev@gmail.com>
2024-05-06 13:14:52 -0500

    time :3

--- src/main.c
+++ src/main.c
@@ -1,51 +1,76 @@
+#include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/utsname.h>
 #include <sys/sysinfo.h>
+#include <sys/utsname.h>
 #include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
+
+typedef struct {
+  long seconds;
+  long minutes;
+  long hours;
+} Time;
+
+static Time get_time(long seconds);
 
 static long get_uptime();
-static char* get_user_hostname();
+static char *get_user_hostname();
+static char *get_model();
 
 int main(void) {
-    char* user_hostname = get_user_hostname();
-    long uptime = get_uptime();
-    printf("%ld\n", uptime);
+  char *user_hostname = get_user_hostname();
 
-    free(user_hostname);
+  free(user_hostname);
 }
 
 // caller takes control of memory, and must free it!
-static char* get_user_hostname() {
-    char* output = malloc(HOST_NAME_MAX + LOGIN_NAME_MAX);
+static char *get_user_hostname() {
+  char *output = malloc(HOST_NAME_MAX + LOGIN_NAME_MAX);
 
-    char login_name[LOGIN_NAME_MAX];
-    int login_res = getlogin_r(login_name, LOGIN_NAME_MAX);
+  char login_name[LOGIN_NAME_MAX];
+  int login_res = getlogin_r(login_name, LOGIN_NAME_MAX);
 
-    char hostname[HOST_NAME_MAX];
-    int host_res = gethostname(hostname, HOST_NAME_MAX);
+  char hostname[HOST_NAME_MAX];
+  int host_res = gethostname(hostname, HOST_NAME_MAX);
 
-    if (login_res != 0 || host_res != 0) {
-        perror("error while getting the hostname");
-        exit(1);
-    }
+  if (login_res != 0 || host_res != 0) {
+    perror("error while getting the hostname");
+    exit(1);
+  }
 
-    snprintf(output, HOST_NAME_MAX + LOGIN_NAME_MAX, "%s@%s", login_name, hostname);
+  snprintf(output, HOST_NAME_MAX + LOGIN_NAME_MAX, "%s@%s", login_name,
+           hostname);
 
-    return output;
+  return output;
 }
 
+static char *get_model() {}
+
 static long get_uptime() {
-    struct sysinfo sys;
+  struct sysinfo sys;
 
-    int res = sysinfo(&sys);
+  int res = sysinfo(&sys);
 
-    if (res != 0) {
-        perror("error while getting sysinfo");
-        exit(1);
-    }
+  if (res != 0) {
+    perror("error while getting sysinfo");
+    exit(1);
+  }
 
-    return sys.uptime;
+  return sys.uptime;
+}
+
+static Time get_time(long seconds) {
+  long seconds_remaining = seconds % 3600;
+
+  seconds -= seconds_remaining;
+  Time time = {.hours = seconds / 3600};
+
+  seconds = seconds_remaining;
+  seconds_remaining = seconds % 60;
+
+  time.minutes = (seconds - seconds_remaining) / 60;
+  time.seconds = seconds_remaining;
+
+  return time;
 }
